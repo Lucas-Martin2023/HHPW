8027cadc2ba1dd3de9739719176a6217256f847c
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2024-04-09 21:30:23 -0500

    pusing up the setup of project

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,6 +1,11 @@
-﻿namespace HHPW.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HHPW.Models;
+
+public class Item
 {
-    public class Item
-    {
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int price { get; set; }
+
 }
